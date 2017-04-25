ac9fe3513466005b6c81941f4b57acab17939f54
morgana4 <morgana4@dhcp-v880-17c-00582.1Xwireless.unc.edu>
2017-04-25 16:39:23 -0400

    yoga timer added

--- app/src/main/java/com/example/morgana4/finalproject/Vriksasana.java
+++ app/src/main/java/com/example/morgana4/finalproject/Vriksasana.java
@@ -2,22 +2,29 @@ package com.example.morgana4.finalproject;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Chronometer;
 
-public class Vriksasana extends AppCompatActivity implements SensorEventListener {
+import java.util.ArrayList;
+
+public class Vriksasana extends AppCompatActivity implements SensorEventListener, Chronometer.OnChronometerTickListener {
 
     Button yogaIntent;
     Button distanceIntent;
     private SensorManager sm;
     private Sensor acc;
+    Chronometer simpleChronometer;
+    ArrayList<Double> valueList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,17 +36,48 @@ public class Vriksasana extends AppCompatActivity implements SensorEventListener
 
         sm = YogaActivity.sm;
         acc = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        sm.registerListener(this, acc, SensorManager.SENSOR_DELAY_NORMAL);
 
         if (sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
             acc = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-            sm.registerListener(this, acc, 1000000);
+            sm.registerListener(this, acc, sm.SENSOR_DELAY_NORMAL);
+
         }
+
+
+        simpleChronometer = (Chronometer) findViewById(R.id.simpleChronometer); // initiate a chronometer
+        simpleChronometer.setBase(SystemClock.elapsedRealtime() + (30000));
+
+         valueList = new ArrayList<Double>();
     }
 
     public void startTracker(View v){
         //start timer and tracking sensor data
+        simpleChronometer.start();
+        simpleChronometer.setBackgroundColor(Color.GREEN);
+        simpleChronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+
+
+            @Override
+            public void onChronometerTick(Chronometer chronometer) {
+                if(chronometer.getText().toString().equalsIgnoreCase("00:00")) {
+                    simpleChronometer.stop();
+                    simpleChronometer.setBackgroundColor(Color.RED);
+                    //give info from sensor
+                }
+            }
+        });
     }
+
+    protected void onResume() {
+        super.onResume();
+        sm.registerListener(this, acc, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        sm.unregisterListener(this);
+    }
+
 
     public void swapHome(View v){
         Log.v("TAG", "swapped to yoga intent");
@@ -48,21 +86,23 @@ public class Vriksasana extends AppCompatActivity implements SensorEventListener
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        sm.registerListener(this, acc, SensorManager.SENSOR_DELAY_NORMAL);
-    }
-
-    @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
         float xVal = sensorEvent.values[0];
         float yVal = sensorEvent.values[1];
         float zVal = sensorEvent.values[2];
         double value = Math.sqrt((xVal*xVal)+(yVal*yVal)+(zVal*zVal));
+        valueList.add(value);
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
 
     }
+
+    @Override
+    public void onChronometerTick(Chronometer chronometer) {
+
+    }
+
+
 }
